5f99e12927d6a02050369dac7445143570a5300d
Tom Jones <tomjones@hiveit.co.uk>
2025-07-11 09:52:26 +0100

    Move healthcheck to separate resource.

--- screen/screen_controller.R
+++ screen/screen_controller.R
@@ -1,10 +1,3 @@
-#* Test the service is running
-#* @serializer unboxedJSON
-#* @get /api/screen
-test_get <- function() {
-    list("Success")
-}
-
 #* Screen data set files located at the supplied blob storage paths using the eesyscreener package
 #* @parser json
 #* @serializer unboxedJSON
