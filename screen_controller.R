dafb7f3bf34a3b455cfb6c9c07dabe1b8daa200e
Rich Bielby <richbielby@e.email>
2025-12-11 12:04:30 +0000

    Running code styling

--- screen_controller.R
+++ screen_controller.R
@@ -36,14 +36,31 @@ screen <- function(req, res) {
     message(
       "Storage account environment variables not set. Using local files: ",
       temp_data_path,
-      " and ", temp_meta_path
+      " and ",
+      temp_meta_path
     )
   } else {
     temp_data_path <- paste0(tempdir(), "/", data_file_name)
     temp_meta_path <- paste0(tempdir(), "/", meta_file_name)
 
-    data_file_url_with_token <- paste0(storage_account_url, "/", blob_container_name, "/", data_file_path, "?", data_file_sas_token)
-    meta_file_url_with_token <- paste0(storage_account_url, "/", blob_container_name, "/", meta_file_path, "?", meta_file_sas_token)
+    data_file_url_with_token <- paste0(
+      storage_account_url,
+      "/",
+      blob_container_name,
+      "/",
+      data_file_path,
+      "?",
+      data_file_sas_token
+    )
+    meta_file_url_with_token <- paste0(
+      storage_account_url,
+      "/",
+      blob_container_name,
+      "/",
+      meta_file_path,
+      "?",
+      meta_file_sas_token
+    )
 
     message(
       "Downloading files from Azure Blob Storage: ",
@@ -55,37 +72,53 @@ screen <- function(req, res) {
 
     result <- tryCatch(
       {
-    h <- new_handle()
-    handle_setheaders(h, "Accept-Encoding" = "gzip, zstd")
+        h <- new_handle()
+        handle_setheaders(h, "Accept-Encoding" = "gzip, zstd")
 
-    curl_download(url = data_file_url_with_token, destfile = temp_data_path, handle = h)
-    curl_download(url = meta_file_url_with_token, destfile = temp_meta_path, handle = h)
+        curl_download(
+          url = data_file_url_with_token,
+          destfile = temp_data_path,
+          handle = h
+        )
+        curl_download(
+          url = meta_file_url_with_token,
+          destfile = temp_meta_path,
+          handle = h
+        )
       },
       error = function(e) {
         res$status <- 400
         res$body <- paste0("Data failed to transfer from blob storage: ", e)
       }
     )
-
   }
 
-  result <- tryCatch({
-    result <- screen_csv(temp_data_path, temp_meta_path, data_file_name, meta_file_name)
+  result <- tryCatch(
+    {
+      result <- screen_csv(
+        temp_data_path,
+        temp_meta_path,
+        data_file_name,
+        meta_file_name
+      )
 
-    # Remove test files if sourcing from blob storage (and not if sourcing from local directory)
-    if (!use_local_storage) {
-      file.remove(temp_data_path)
-      file.remove(temp_meta_path)
+      # Remove test files if sourcing from blob storage (and not if sourcing from local directory)
+      if (!use_local_storage) {
+        file.remove(temp_data_path)
+        file.remove(temp_meta_path)
+      }
+
+      res$status <- 200
+      res$body <- result
+    },
+    error = function(e) {
+      print(paste0("Error details: ", e))
+      res$status <- 400
+      res$body <- paste0("An unhandled exception occurred in eesyscreener: ", e)
+      # TODO: Add logging
+    },
+    finally = {
+      # Intentionally blank
     }
-
-    res$status <- 200
-    res$body <- result
-  }, error = function(e) {
-    print(paste0("Error details: ", e))
-    res$status <- 400
-    res$body <- paste0("An unhandled exception occurred in eesyscreener: ", e)
-    # TODO: Add logging
-  }, finally = {
-    # Intentionally blank
-  })
+  )
 }
