d92c746fda97ca04663aa0bf33512f3cc1b683f8
Duncan Watson <duncan@hiveit.co.uk>
2026-04-17 15:46:31 +0100

    EES-7002 - updated progress check and delete endpoints to accept multiple data set ids rather than single ones

--- src/function_handlers/handle_check_progress.R
+++ src/function_handlers/handle_check_progress.R
@@ -1,32 +1,29 @@
-#* Check the screening progress for a particular data set.
+#* Check the screening progress for a particular set of data sets.
 handle_check_progress <- function(req, res) {
 
   source(here::here("src/services/screener_progress.R"))
   
-  data_set_ids <- req$args$dataSetId
+  data_set_ids <- req$args$data_set_id
 
-  if (is.null(data_set_ids) or length(data_set_ids) == 0) {
+  if (is.null(data_set_ids) || length(data_set_ids) == 0) {
     res$status <- 400
     return(list(
-      message = "Query parameter dataSetId must be specified."
+      message = "Query parameter data_set_id must be specified."
     ))
   }
 
-  lapply(data_set_ids, function(data_set_id) {
+  Filter(Negate(is.null), lapply(data_set_ids, function(data_set_id) {
     progress = check_progress(data_set_id)
 
     if (is.null(progress)) {
-      res$status <- 404
-      return(list(
-        message = paste0("A progress file for dataSetId \"", data_set_id, "\" was not found.")
-      ))
+      return();
     }
-
+    
     res$status <- 200
     res$body <- list(
       data_set_id = data_set_id,
       percentage_complete = progress$progress,
       stage = progress$status[1]
     )
-  })
+  }))
 }
--- tests/testthat/test-api-check-progress.R
+++ tests/testthat/test-api-check-progress.R
@@ -2,7 +2,7 @@ library(jsonlite)
 
 source("utils/progress_file_test_utils.R")
 
-testthat::test_that("GET to the progress function without a dataSetId returns a 400", {
+testthat::test_that("GET to the progress function without a data_set_id returns a 400", {
   request <- api_url(api_host(), api_port()) |>
     httr2::request() |>
     httr2::req_error(is_error = function(resp) FALSE) |>
@@ -15,29 +15,10 @@ testthat::test_that("GET to the progress function without a dataSetId returns a 
 
   result <- httr2::resp_body_json(resp)
 
-  expect_equal(result$message, "Query parameter dataSetId must be specified.")
+  expect_equal(result$message, "Query parameter data_set_id must be specified.")
 })
 
-
-testthat::test_that("GET to the progress function with a dataSetId for a non-existent progress file returns a 404", {
-  request <- api_url(api_host(), api_port()) |>
-    httr2::request() |>
-    httr2::req_error(is_error = function(resp) FALSE) |>
-    httr2::req_url_path("api/progress") |>
-    httr2::req_url_query(dataSetId = "not_found") |>
-    httr2::req_method("GET")
-
-  resp <- httr2::req_perform(request)
-
-  expect_equal(httr2::resp_status(resp), 404)
-
-  result <- httr2::resp_body_json(resp)
-
-  expect_equal(result$message, "A progress file for dataSetId \"not_found\" was not found.")
-})
-
-
-testthat::test_that("GET to the progress function with a dataSetId for an existing progress file returns the current progress", {
+testthat::test_that("GET to the progress function with a data_set_id for an existing progress file returns the current progress", {
   
   data_set_id = "existing"
   percentage_complete = 90.12
@@ -49,7 +30,7 @@ testthat::test_that("GET to the progress function with a dataSetId for an existi
   request <- api_url(api_host(), api_port()) |>
     httr2::request() |>
     httr2::req_url_path("api/progress") |>
-    httr2::req_url_query(dataSetId = data_set_id) |>
+    httr2::req_url_query(data_set_id = data_set_id) |>
     httr2::req_method("GET")
 
   resp <- httr2::req_perform(request)
@@ -60,12 +41,12 @@ testthat::test_that("GET to the progress function with a dataSetId for an existi
 
   expect_equal(length(result), 1)
 
-  expect_equal(result[1]$dataSetId, data_set_id)
-  expect_equal(result[1]$percentComplete, percentage_complete)
-  expect_equal(result[1]$stage, stage)
+  expect_equal(result[[1]]$data_set_id, data_set_id)
+  expect_equal(result[[1]]$percentage_complete, percentage_complete)
+  expect_equal(result[[1]]$stage, stage)
 })
 
-testthat::test_that("GET to the progress function with multiple dataSetIds for existing progress files return the current progress", {
+testthat::test_that("GET to the progress function with multiple data_set_ids for existing progress files return the current progress", {
   
   data_set_1_id = "existing_1"
   data_set_1_percentage_complete = 90.12
@@ -82,7 +63,7 @@ testthat::test_that("GET to the progress function with multiple dataSetIds for e
   request <- api_url(api_host(), api_port()) |>
     httr2::request() |>
     httr2::req_url_path("api/progress") |>
-    httr2::req_url_query(dataSetId = c(data_set_1_id, data_set_2_id), .multi = "explode") |>
+    httr2::req_url_query(data_set_id = c(data_set_1_id, data_set_2_id), .multi = "explode") |>
     httr2::req_method("GET")
 
   resp <- httr2::req_perform(request)
@@ -93,11 +74,41 @@ testthat::test_that("GET to the progress function with multiple dataSetIds for e
 
   expect_equal(length(result), 2)
 
-  expect_equal(result[1]$dataSetId, data_set_1_id)
-  expect_equal(result[1]$percentComplete, data_set_1_percentage_complete)
-  expect_equal(result[1]$stage, data_set_1_stage)
+  expect_equal(result[[1]]$data_set_id, data_set_1_id)
+  expect_equal(result[[1]]$percentage_complete, data_set_1_percentage_complete)
+  expect_equal(result[[1]]$stage, data_set_1_stage)
 
-  expect_equal(result[2]$dataSetId, data_set_2_id)
-  expect_equal(result[2]$percentComplete, data_set_2_percentage_complete)
-  expect_equal(result[2]$stage, data_set_2_stage)
+  expect_equal(result[[2]]$data_set_id, data_set_2_id)
+  expect_equal(result[[2]]$percentage_complete, data_set_2_percentage_complete)
+  expect_equal(result[[2]]$stage, data_set_2_stage)
+})
+
+testthat::test_that("GET to the progress function with a data_set_id for an existing progress file and a non-existent one returns the existing progress and ignores the not found one", {
+  
+  data_set_id = "existing"
+  percentage_complete = 90.12
+  stage = "started"
+
+  non_existent_data_set_id = "non-existent"
+
+  # Create a temporary existing progress file.
+  create_progress_file(data_set_id, percentage_complete, stage)
+
+  request <- api_url(api_host(), api_port()) |>
+    httr2::request() |>
+    httr2::req_url_path("api/progress") |>
+    httr2::req_url_query(data_set_id = c(data_set_id, non_existent_data_set_id), .multi = "explode") |>
+    httr2::req_method("GET")
+
+  resp <- httr2::req_perform(request)
+
+  expect_equal(httr2::resp_status(resp), 200)
+
+  result <- httr2::resp_body_json(resp)
+
+  expect_equal(length(result), 1)
+
+  expect_equal(result[[1]]$data_set_id, data_set_id)
+  expect_equal(result[[1]]$percentage_complete, percentage_complete)
+  expect_equal(result[[1]]$stage, stage)
 })
