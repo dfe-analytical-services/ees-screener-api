6d1b6a71ba325339c5efdc92e8331c0674575897
Duncan Watson <duncan@hiveit.co.uk>
2026-05-12 17:31:25 +0100

    EES-7003 - explicitly converting concurrent workers config to integer

--- run.R
+++ run.R
@@ -6,7 +6,7 @@ source(here::here("src/utils/stdout_log_appender.R"))
 
 # Set up a pool of background workers to be used by code blocks that execute
 # as futures (e.g. long-running background requests).
-number_of_concurrent_workers <- Sys.getenv("CONCURRENT_R_WORKERS", unset = 0)
+number_of_concurrent_workers <- strtoi(Sys.getenv("CONCURRENT_R_WORKERS", unset = 0))
 
 if (number_of_concurrent_workers > 0) {
   plan(multisession, workers = number_of_concurrent_workers)
