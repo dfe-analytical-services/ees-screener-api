d4511bc21b8477e643777885ae50ea2b6e658730
Duncan Watson <duncan@hiveit.co.uk>
2026-06-29 18:28:35 +0100

    EES-7139 - removing old foreground screener code in favour of background screening. Updated README with details of the new screening behaviour.

--- routes.R
+++ routes.R
@@ -6,14 +6,6 @@ healthcheck_route <- function() {
   handle_healthcheck()
 }
 
-#* @parser json
-#* @serializer unboxedJSON
-#* @post /api/screen
-screen_route <- function(req, res) {
-  source(here::here("src/function_handlers/handle_screen.R"))
-  handle_screen(req, res)
-}
-
 #* Note that routes for queue-triggered Functions are enforced by the
 #* Azure Functions host and runtime, and always take the name of the
 #* folder in which the function.json for the Function exists, hence
